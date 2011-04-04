7eb7e5d9a01d73fa3a9c967c6ff7023b45bcc114
Alfred E. Heggestad <aeh@db.org>
2011-04-04 22:24:20 +0000

    resamp: use FIR filter for both up/down sampling

--- include/rem_auresamp.h
+++ include/rem_auresamp.h
@@ -6,7 +6,8 @@
 
 struct auresamp;
 
-int auresamp_alloc(struct auresamp **arp, int channels, double ratio);
+int auresamp_alloc(struct auresamp **arp, int channels,
+		   uint32_t srate_in, uint32_t srate_out);
 int auresamp_process(struct auresamp *ar, struct mbuf *mb);
 
 
--- src/auresamp/resamp.c
+++ src/auresamp/resamp.c
@@ -5,29 +5,23 @@
 #include "fir.h"
 
 
-enum { FIR_SIZE = 16 };
-
-
 struct auresamp {
-	int16_t hist[FIR_SIZE + 2];
+	struct fir fir;
+	const int16_t *coeffv;
+	int coeffn;
 	int channels;
 	double ratio;
-	struct fir fir;
 };
 
 
-static const int16_t fireven[FIR_SIZE] = {
-	  -1,   26,   89, -26,
-	 -15,   77, -175, 404,
-	 777,  -16,  -67,  81,
-	 -71,   54,   66,   3
-};
-
-static const int16_t firodd[FIR_SIZE] = {
-	   3,   66,  54, -71,
-	  81,  -67, -16, 777,
-	 404, -175,  77, -15,
-	 -26,   89,  26,  -1
+/*
+ * FIR filter with 31 taps, cutoff 8000Hz, samplerate 16000Hz
+ */
+static const int16_t fir_lowpass[31] = {
+  -55,      0,     96,      0,   -220,      0,    461,      0,  
+ -877,      0,   1608,      0,  -3176,      0,  10342,  16410,  
+10342,      0,  -3176,      0,   1608,      0,   -877,      0,  
+  461,      0,   -220,      0,     96,      0,    -55,  
 };
 
 
@@ -43,36 +37,15 @@ static inline int16_t saturate_s16(int32_t a)
 
 
 static inline void upsample(struct auresamp *ar, int16_t *dst,
-			    const int16_t *src, size_t nsamp)
+			    const int16_t *src, size_t nsamp_dst)
 {
-	int ratio = (int)ar->ratio;
-	int i;
+	size_t i;
+	double p = 0;
 
-	while (nsamp--) {
+	for (i=0; i < nsamp_dst; i++) {
 
-		int32_t sumv[ratio];
-		const int16_t *hp = ar->hist, *hp_max = hp + FIR_SIZE;
-		const int16_t *cpe = fireven, *cpo = firodd;
-
-		memset(sumv, 0, sizeof(sumv));
-
-		memmove(ar->hist + 1, ar->hist, (FIR_SIZE - 1) * 2);
-		ar->hist[0] = *src++;
-
-		for (;hp < hp_max; hp++) {
-
-			for (i=0; i<ratio/2; i++) {
-
-				sumv[2*i]   += (int32_t)(*cpe) * (int32_t)*hp;
-				sumv[2*i+1] += (int32_t)(*cpo) * (int32_t)*hp;
-			}
-
-			++cpe;
-			++cpo;
-		}
-
-		for (i=0; i<ratio; i++)
-			*dst++ = saturate_s16(sumv[i] >> 10);
+		*dst++ = src[(int)p];
+		p += 1/ar->ratio;
 	}
 }
 
@@ -89,23 +62,36 @@ static inline void downsample(struct auresamp *ar, int16_t *dst,
 }
 
 
-int auresamp_alloc(struct auresamp **arp, int channels, double ratio)
+int auresamp_alloc(struct auresamp **arp, int channels,
+		   uint32_t srate_in, uint32_t srate_out)
 {
 	struct auresamp *ar;
 
-	if (!arp || channels != 1 || ratio == 0)
+	if (!arp || channels != 1 || !srate_in || !srate_out)
 		return EINVAL;
 
-	re_printf("allocate resampler: ratio=%f\n", ratio);
+	re_printf("allocate resampler: %uHz ---> %uHz\n",
+		  srate_in, srate_out);
 
 	ar = mem_zalloc(sizeof(*ar), NULL);
 	if (!ar)
 		return ENOMEM;
 
 	ar->channels = channels;
-	ar->ratio = ratio;
+	ar->ratio = 1.0 * srate_out / srate_in;
 
 	fir_init(&ar->fir);
+
+	if (ar->ratio < 1) {
+
+		ar->coeffv = fir_lowpass;
+		ar->coeffn = (int)ARRAY_SIZE(fir_lowpass);
+	}
+	else if (ar->ratio > 1) {
+
+		ar->coeffv = fir_lowpass;
+		ar->coeffn = (int)ARRAY_SIZE(fir_lowpass);
+	}
 
 	*arp = ar;
 
@@ -124,6 +110,9 @@ int auresamp_process(struct auresamp *ar, struct mbuf *mb)
 	if (!ar || !mb)
 		return EINVAL;
 
+	if (ar->ratio == 1)
+		return 0;
+
 	buf = mem_alloc(sz, NULL);
 	if (!buf)
 		return ENOMEM;
@@ -132,9 +121,15 @@ int auresamp_process(struct auresamp *ar, struct mbuf *mb)
 
 	if (ar->ratio > 1) {
 
-		upsample(ar, buf, (void *)mbuf_buf(mb), nsamp);
+		upsample(ar, buf, (void *)mbuf_buf(mb), nsamp_out);
+
+		mb->pos = pos;
+
+		auresamp_lowpass(ar, mb);
 	}
 	else {
+		/* decimation: low-pass filter, then downsample */
+
 		auresamp_lowpass(ar, mb);
 
 		mb->pos = pos;
@@ -153,23 +148,6 @@ int auresamp_process(struct auresamp *ar, struct mbuf *mb)
 }
 
 
-// bandpass filter centred around 1000 Hz
-// sampling rate = 8000 Hz
-// gain at 1000 Hz is about 1.13
-
-#define FILTER_LEN  63
-static const int16_t coeffs[ FILTER_LEN ] = {
-	-1468, 1058,   594,   287,    186,  284,   485,   613,
-	495,   90,  -435,  -762,   -615,   21,   821,  1269,
-	982,    9, -1132, -1721,  -1296,    1,  1445,  2136,
-	1570,    0, -1666, -2413,  -1735,   -2,  1770,  2512,
-	1770,   -2, -1735, -2413,  -1666,    0,  1570,  2136,
-	1445,    1, -1296, -1721,  -1132,    9,   982,  1269,
-	821,   21,  -615,  -762,   -435,   90,   495,   613,
-	485,  284,   186,   287,    594, 1058, -1468
-};
-
-
 int auresamp_lowpass(struct auresamp *ar, struct mbuf *mb)
 {
 	const int16_t *src;
@@ -186,7 +164,7 @@ int auresamp_lowpass(struct auresamp *ar, struct mbuf *mb)
 
 		int len = min(nsamp, FIR_MAX_INPUT_LEN);
 
-		fir_process(&ar->fir, coeffs, src, dst, len, FILTER_LEN);
+		fir_process(&ar->fir, ar->coeffv, src, dst, len, ar->coeffn);
 
 		src   += len;
 		dst   += len;
