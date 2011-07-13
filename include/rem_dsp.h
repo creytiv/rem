78885da80f5a89933834a6e1dce17d136b3c7815
Alfred E. Heggestad <aeh@db.org>
2011-07-13 14:29:31 +0000

    add support for Symbian

--- include/rem_dsp.h
+++ include/rem_dsp.h
@@ -1,6 +1,15 @@
 /* DSP routines */
 
 
+#ifndef INT16_MAX
+#define INT16_MAX 0x7fff
+#endif
+
+#ifndef INT16_MIN
+#define INT16_MIN (-INT16_MAX - 1)
+#endif
+
+
 static inline uint8_t saturate_u8(int a)
 {
 	return ((a > 255) ? 255 : ((a < 0) ? 0 : a));
@@ -18,7 +27,7 @@ static inline int16_t saturate_s16(int32_t a)
 }
 
 
-#ifdef __arm__
+#if defined __arm__ && !defined __SYMBIAN32__
 static inline int16_t sadd16(int a, int b)
 {
 	__asm__ __volatile__ ("add %0, %1, %2"   "\n\t"
