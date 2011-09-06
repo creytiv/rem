855a97d2c4a34f4bfc7004a43879724403c19070
Alfred E. Heggestad <aeh@db.org>
2011-09-06 13:02:09 +0000

    vidconv: remove unused flags

--- include/rem_vidconv.h
+++ include/rem_vidconv.h
@@ -5,16 +5,6 @@
  */
 
 
-/** Vidconv flags */
-enum {
-	VIDCONV_HFLIP     = 1<<0,
-	VIDCONV_VFLIP     = 1<<1,
-	VIDCONV_ROTATE90  = 1<<2,
-	VIDCONV_ROTATE180 = VIDCONV_HFLIP | VIDCONV_VFLIP,
-	VIDCONV_ROTATE270 = VIDCONV_ROTATE180 | VIDCONV_ROTATE90
-};
-
-
 void vidconv(struct vidframe *dst, const struct vidframe *src,
 	     struct vidrect *r);
 void vidconv_aspect(struct vidframe *dst, const struct vidframe *src,
