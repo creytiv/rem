0bda27ab1fb4d5187761899edaf2b0944451fc7f
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2021-05-23 16:36:20 +0200

    h264: add functions from baresip

--- include/rem_h264.h
+++ include/rem_h264.h
@@ -73,3 +73,30 @@ struct vidsz;
 int  h264_sps_decode(struct h264_sps *sps, const uint8_t *p, size_t len);
 void h264_sps_resolution(const struct h264_sps *sps, struct vidsz *sz);
 const char *h264_sps_chroma_format_name(uint8_t chroma_format_idc);
+
+
+typedef int (h264_packet_h)(bool marker, uint64_t rtp_ts,
+			    const uint8_t *hdr, size_t hdr_len,
+			    const uint8_t *pld, size_t pld_len,
+			    void *arg);
+
+/** Fragmentation Unit header */
+struct h264_fu {
+	unsigned s:1;      /**< Start bit                               */
+	unsigned e:1;      /**< End bit                                 */
+	unsigned r:1;      /**< The Reserved bit MUST be equal to 0     */
+	unsigned type:5;   /**< The NAL unit payload type               */
+};
+
+int h264_fu_hdr_encode(const struct h264_fu *fu, struct mbuf *mb);
+int h264_fu_hdr_decode(struct h264_fu *fu, struct mbuf *mb);
+
+const uint8_t *h264_find_startcode(const uint8_t *p, const uint8_t *end);
+
+int h264_packetize(uint64_t rtp_ts, const uint8_t *buf, size_t len,
+		   size_t pktsize, h264_packet_h *pkth, void *arg);
+int h264_nal_send(bool first, bool last,
+		  bool marker, uint32_t ihdr, uint64_t rtp_ts,
+		  const uint8_t *buf, size_t size, size_t maxsz,
+		  h264_packet_h *pkth, void *arg);
+bool h264_is_keyframe(int type);
