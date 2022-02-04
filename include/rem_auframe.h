5b920d0f11fc59e0dc9e2ef55eafecaf35da24a2
Sebastian Reimers <service@it-sreimers.de>
2022-02-04 10:52:32 +0100

    auframe: add auframe_update (#19)

--- include/rem_auframe.h
+++ include/rem_auframe.h
@@ -16,5 +16,25 @@ struct auframe {
 
 void auframe_init(struct auframe *af, enum aufmt fmt, void *sampv,
 		  size_t sampc, uint32_t srate, uint8_t ch);
+
+/**
+ * Update an audio frame
+ *
+ * @param af        Audio frame
+ * @param sampv     Audio samples
+ * @param sampc     Total number of audio samples
+ * @param timestamp Timestamp in AUDIO_TIMEBASE units
+ */
+static inline void auframe_update(struct auframe *af, void *sampv,
+				  size_t sampc, uint64_t timestamp)
+{
+	if (!af)
+		return;
+
+	af->sampv = sampv;
+	af->sampc = sampc;
+	af->timestamp = timestamp;
+}
+
 size_t auframe_size(const struct auframe *af);
 void   auframe_mute(struct auframe *af);
